6c5f13db4864849e1c4bbb3c9ef60333bb6f839b
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-04-30 18:05:19 +0100

    Fix imports

--- plants/views.py
+++ plants/views.py
@@ -1,34 +1,39 @@
-import serializers
-
+# isort: skip_file
 from helpers.views import BaseAPIView
 
 from .managers import BasePlantsManager
 from .models import Cultivar, Family, Genus, Species
+from .serializers import (
+    CultivarSerializer,
+    FamilySerializer,
+    GenusSerializer,
+    SpeciesSerializer,
+)
 
 
 class FamilyView(BaseAPIView):
     class Meta(BaseAPIView.Meta):
         model = Family
-        serializer = serializers.FamilySerializer
+        serializer = FamilySerializer
         manager = BasePlantsManager(model, serializer)
 
 
 class GenusView(BaseAPIView):
     class Meta(BaseAPIView.Meta):
         model = Genus
-        serializer = serializers.GenusSerializer
+        serializer = GenusSerializer
         manager = BasePlantsManager(model, serializer)
 
 
 class SpeciesView(BaseAPIView):
     class Meta(BaseAPIView.Meta):
         model = Species
-        serializer = serializers.SpeciesSerializer
+        serializer = SpeciesSerializer
         manager = BasePlantsManager(model, serializer)
 
 
 class CultivarView(BaseAPIView):
     class Meta(BaseAPIView.Meta):
         model = Cultivar
-        serializer = serializers.CultivarSerializer
+        serializer = CultivarSerializer
         manager = BasePlantsManager(model, serializer)
